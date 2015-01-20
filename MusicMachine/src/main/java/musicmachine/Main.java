e7c37eed392a66a25db6a2b79299728184be25f3
Arttu Ylhävuori <areee@users.noreply.github.com>
2015-01-20 11:36:05 +0200

    luokkia päivitetty

--- MusicMachine/src/main/java/musicmachine/Main.java
+++ MusicMachine/src/main/java/musicmachine/Main.java
@@ -1,11 +1,14 @@
 package musicmachine;
 
-
+import java.util.Scanner;
+import musicmachine.logic.Sovelluslogiikka;
 
 public class Main {
 
     public static void main(String[] args) {
-        
+        Scanner lukija = new Scanner(System.in);
+
+        Sovelluslogiikka.kaynnista(lukija);
     }
 
 }
--- MusicMachine/src/main/java/musicmachine/logic/Sovelluslogiikka.java
+++ MusicMachine/src/main/java/musicmachine/logic/Sovelluslogiikka.java
@@ -1,5 +1,11 @@
 package musicmachine.logic;
 
+import java.util.Scanner;
+
 public class Sovelluslogiikka {
+
+    public static void kaynnista(Scanner lukija) {
+        
+    }
     
 }
